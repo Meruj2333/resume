f25a5da947ac638ab4b1423842b3321301e76b90
Meruj2333 <merujharutyunyan0620@gmail.com>
2025-11-17 09:11:52 +0400

    boxShadow

--- pages/box-shadow/script.js
+++ pages/box-shadow/script.js
@@ -1,80 +1,137 @@
 $=s=>document.querySelector(s)
-const boxEl=$('#box')
-$('#coverColor').oninput=function(e){
-    $('.cover-bg').style.background=e.target.value;
+const styled = {
+    main:[
+        {
+            el:'#coverColor',
+            property:'background',
+            unit:'',
+            to:'.cover-bg'
+        },
+        {
+            el:'#boxColor',
+            property:'background',
+            unit:'',
+            to:'#box'
+        },
+        {
+            el:'#boxW',
+            property:'width',
+            unit:'px',
+            to:'#box'
+        },
+        {
+            el:'#boxH',
+            property:'height',
+            unit:'px',
+            to:'#box'
+        },
+        {
+            el:'#borderW',
+            property:'borderWidth',
+            unit:'px',
+            to:'#box'
+        },
+        {
+            el:'#borderR',
+            property:'borderRadius',
+            unit:'px',
+            to:'#box'
+        },
+        {
+            el:'#borderC',
+            property:'borderColor',
+            unit:'',
+            to:'#box'
+        },
+        {
+            el:'#textColor',
+            property:'color',
+            unit:'',
+            to:'#box'
+        },
+        {
+            el:'#fontSize',
+            property:'fontSize',
+            unit:'px',
+            to:'#box'
+        },
+        {
+            el:'#fontWeight',
+            property:'fontWeight',
+            unit:'',
+            to:'#box'
+        },
+        {
+            el:'#textTransform',
+            property:'textTransform',
+            unit:'',
+            to:'#box'
+        },
+        {
+            el:'#textDecoration',
+            property:'textDecoration',
+            unit:'',
+            to:'#box'
+        },
+        {
+            el:'#rotate',
+            property:'rotate',
+            unit:'deg',
+            to:'#box'
+        },
+        {
+            el:'#scale',
+            property:'scale',
+            unit:'',
+            to:'#box'
+        },
+        {
+            el:'#transitionTime',
+            property:'transitionDuration',
+            unit:'s',
+            to:'#box'
+        }
+    ],
+    shadow:{
+       values:['#000000','0px','0px','0px','0px'],
+        to:'#box',
+        elements:[
+            {
+                el:'#shColor',
+                index:0,
+                unit:'',
+            },
+            {
+                el:'#shX',
+                index:1,
+                unit:'px',
+            },
+            {
+                el:'#shY',
+                index:2,
+                unit:'px',
+            },
+            {
+                el:'#shBlur',
+                index:3,
+                unit:'px',
+            },
+            {
+                el:'#shSpread',
+                index:4,
+                unit:'px',
+            }
+        ]
+    }
 }
-$('#boxW').oninput=function(e){
-    boxEl.style.width=e.target.value+"px"
-    $('#boxWNum').value=e.target.value;
-
-}
-$('#boxWNum').oninput=function(e){
-    boxEl.style.width=e.target.value+"px"
-    $('#boxW').value=e.target.value;
-
-}
-$('#boxH').oninput=function(e){
-    boxEl.style.height=e.target.value+"px"
-    $('#boxHNum').value=e.target.value;
-
-}
-$('#boxHNum').oninput=function(e){
-    boxEl.style.width=e.target.value+"px"
-    $('#boxW').value=e.target.value;
-
-}
-$('#borderW').oninput=function(e){
-    boxEl.style.borderWidth=e.target.value+"px"
-    $('#borderWNum').value=e.target.value;
-
-}
-$('#borderWNum').oninput=function(e){
-    boxEl.style.width=e.target.value+"px"
-    $('#borderW').value=e.target.value;
-
-}
-$('#borderC').oninput=function(e){
-    $('#box').style.borderColor=e.target.value;
-}
-$('#borderR').oninput=function(e){
-    boxEl.style.borderRadius=e.target.value+"px"
-    $('#borderRNum').value=e.target.value;
-
-}
-$('#borderRNum').oninput=function(e){
-    boxEl.style.width=e.target.value+"px"
-    $('#borderR').value=e.target.value;
-
-}
-const shadow = {
-    x: 0,
-    y: 0,
-    blur: 20,
-    spread: 0,
-    color: '#000000'
-};
-
-function updateShadow() {
-    const { x, y, blur, spread, color } = shadow;
-    boxEl.style.boxShadow = `${x}px ${y}px ${blur}px ${spread}px ${color}`;
-}
-
-function bindInput(idSlider, idNumber, key) {
-    $(idSlider).oninput = function (e) {
-        shadow[key] = e.target.value;
-            $(idNumber).value = shadow[key];
-        updateShadow();
-    };
-
-    $(idNumber).oninput = function (e) {
-        shadow[key] = e.target.value;
-        $(idSlider).value = shadow[key];
-        updateShadow();
-    };
-}
-
-bindInput('#shX', '#shXNum', 'x');
-bindInput('#shY', '#shYNum', 'y');
-bindInput('#shBlur', '#shBlurNum', 'blur');
-bindInput('#shSpread', '#shSpreadNum', 'spread');
-bindInput('#shColor', '#shColor', 'color');
+styled.main.forEach(obj=>{
+$(obj.el).oninput=function(e){
+     $(obj.to).style[obj.property]=e.target.value+obj.unit;
+ }
+})
+styled.shadow.elements.forEach(obj=>{
+    $(obj.el).oninput=function(e){
+        styled.shadow.values[obj.index]=e.target.value+obj.unit;
+        $(styled.shadow.to).style.boxShadow=styled.shadow.values.join(' ')
+    }
+})
